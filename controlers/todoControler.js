fc62148e4560e93e54ed349d67cf227cc7a238bf
zile028 <zile028@gmail.com>
2023-10-20 19:28:12 +0200

    deploy

--- controlers/todoControler.js
+++ controlers/todoControler.js
@@ -1,23 +1,23 @@
 const TodoModel = require("../models/TodoModel");
 const addTodo = (req, res) => {
-    let newTodo = new TodoModel(req.body);
-    newTodo.save()
-        .then((result) => {
-            console.log(result);
-            res.send("saved");
-        });
+  let newTodo = new TodoModel(req.body);
+  newTodo.save().then((result) => {
+    console.log(result);
+    res.send("saved");
+  });
 };
 
 const getAll = (req, res) => {
-    TodoModel.find({}).populate("_user")
-        .then((result) => {
-            console.log(result);
-            res.send("OK");
-        })
-        .catch((error) => {
-            console.log(error);
-            res.send("Error");
-        });
+  TodoModel.find({})
+    .populate("_user")
+    .then((result) => {
+      console.log(result);
+      res.send(result);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.send("Error");
+    });
 };
 
-module.exports = {addTodo, getAll};
+module.exports = { addTodo, getAll };
